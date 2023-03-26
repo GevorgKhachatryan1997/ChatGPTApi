1fc5c90cf1fb7c811a45e3dafb6266783d72b1cb
Artavazd Khachatryan <artavazd575@gmail.com>
2023-03-26 17:42:52 +0400

    Use application manager with diferent scope for claning app data

--- app/src/main/java/com/chatgpt/letaithink/ui/viewModel/SettingViewModel.kt
+++ app/src/main/java/com/chatgpt/letaithink/ui/viewModel/SettingViewModel.kt
@@ -2,12 +2,8 @@ package com.chatgpt.letaithink.ui.viewModel
 
 import android.content.Context
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.chatgpt.letaithink.data.ApiKeyRepository
-import com.chatgpt.letaithink.data.ChatRepository
-import com.chatgpt.letaithink.data.UserRepository
+import com.chatgpt.letaithink.domain.ApplicationManager
 import com.chatgpt.letaithink.domain.GoogleAuthenticationHelper
-import kotlinx.coroutines.launch
 
 class SettingViewModel : ViewModel() {
 
@@ -15,10 +11,6 @@ class SettingViewModel : ViewModel() {
 
     fun onLogoutClick(context: Context) {
         googleAuthenticationHelper.signOutRequest(context)
-        viewModelScope.launch {
-            UserRepository.deleteUserData()
-            ApiKeyRepository.deleteApiKey()
-            ChatRepository.deleteAllData()
-        }
+        ApplicationManager.deleteAllData()
     }
 }
