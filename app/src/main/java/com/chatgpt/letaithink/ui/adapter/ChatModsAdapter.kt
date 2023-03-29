e0ed994130c750b4968d7ee06dd9d67652fe844b
Artavazd Khachatryan <artavazd575@gmail.com>
2023-03-29 10:40:02 +0400

    Update chats screen

--- app/src/main/java/com/chatgpt/letaithink/ui/adapter/ChatModsAdapter.kt
+++ app/src/main/java/com/chatgpt/letaithink/ui/adapter/ChatModsAdapter.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
@@ -57,11 +58,13 @@ class ChatModsAdapter : RecyclerView.Adapter<ChatModsAdapter.ChatModeViewHolder>
                 ivChatMode.setImageResource(image)
                 tvModeTitle.setText(title)
 
-                if (selected) {
-                    clModeContainer.setBackgroundResource(R.color.icon_pressed_color)
+                val backgroundColor = if (selected) {
+                    R.color.icon_pressed_color
                 } else {
-                    clModeContainer.setBackgroundResource(android.R.color.white)
+                    android.R.color.white
                 }
+
+                clModeContainer.setBackgroundColor(itemView.context.getColor(backgroundColor))
             }
         }
     }
