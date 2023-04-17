2a0192248f2cb1f0aefeea1b0d3c71cd43e28073
Artavazd Khachatryan <artavazd575@gmail.com>
2023-04-17 23:44:29 +0400

    Show only email applications for support

--- app/src/main/java/com/chatgpt/letaithink/utils/EmailUtils.kt
+++ app/src/main/java/com/chatgpt/letaithink/utils/EmailUtils.kt
@@ -2,17 +2,22 @@ package com.chatgpt.letaithink.utils
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
+import android.widget.Toast
+import com.chatgpt.letaithink.R
 
 object EmailUtils {
     const val SUPPORT_EMAIL = "letaithink@gmail.com"
 
     fun composeEmail(context: Context) {
-        val intent = Intent(Intent.ACTION_SEND).apply {
+        val intent = Intent(Intent.ACTION_SENDTO).apply {
+            data = Uri.parse("mailto:")
             putExtra(Intent.EXTRA_EMAIL, arrayOf(SUPPORT_EMAIL))
-            type = "plain/text"
         }
         if (intent.resolveActivity(context.packageManager) != null) {
             context.startActivity(intent)
+        } else {
+            Toast.makeText(context, R.string.email_application_not_found, Toast.LENGTH_LONG).show()
         }
     }
 }
